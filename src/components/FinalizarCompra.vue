f15ed1cf0d542183648d08a2afc90d90eb28e411
Wilson Montelo <wilson.montelo@vibedesenv.com>
2019-11-29 19:22:17 -0300

    quarto commit

---
+++ src/components/FinalizarCompra.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>Finalizar compra</p>
+</template>
+
+<script>
+
+import UsuarioForm from "@/components/UsuarioForm.vue";
+
+export default {
+  name: "FinalizarCompra",
+  components: {
+    UsuarioForm
+  },
+  props : ["produtos"]
+}
+</script>
+
+<style>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { api } from "@/services.js";
+import { Object } from "core-js";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
     login: false,
     usuario: {
@@ -17,22 +19,54 @@ export default new Vuex.Store({
       bairro: "",
       cidade: "",
       estado: ""
-    }
+    },
+    usuario_produtos: null
   },
   mutations: {
     UPDATE_LOGIN(state, payload) {
       state.login = payload;
     },
     UPDATE_USUARIO(state, payload) {
-      state.usuario = payload;
+      state.usuario = Object.assign(state.usuario, payload);
+    },
+    UPDATE_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos = payload;
+    },
+    ADD_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos.unshit(payload);
     }
   },
   actions: {
+    getUsuarioProdutos(context) {
+      api.get(`/produto?usuario_id=${context.state.usuario.id}`)
+      .then(r => {
+        context.commit("UPDATE_USUARIO_PRODUTOS", r.data)
+      })
+    },
     getUsuario(context, payload) {
-      api.get(`/usuario/${payload}`).then(response => {
+      return api.get(`/usuario/${payload}`).then(response => {
         context.commit("UPDATE_USUARIO", response.data);
         context.commit("UPDATE_LOGIN", true);
       });
+    },
+    criarUsuario(context, payload) {
+      context.commit("UPDATE_USUARIO", { id: payload.email });
+      return api.post("/usuario", payload);
+    },
+    deslogarUsuario(context) {
+      context.commit("UPDATE_USUARIO", {
+        id: "",
+        nome: "",
+        email: "",
+        senha: "",
+        cep: "",
+        rua: "",
+        numero: "",
+        bairro: "",
+        cidade: "",
+        estado: ""
+      });
+      context.commit("UPDATE_LOGIN", false);
     }
   }
 });
